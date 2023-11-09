b000d75f18940913a34b1aac94921ddb9f507a7a
Jurgen Mane <jurgenmane@gmail.com>
2023-11-08 19:56:35 -0600

    Adding helpers folder

---
+++ helpers/help.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    sum = a + b
+    return  sum
+
+def print_hello(name):
+    print(f"Hello {name}")
+
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("HELLO WORLD")
-print("HELLO WORLD")
-print("HELLO WORLD")
+from helpers.help import print_hello
+
+def main():
+    user_name = input("What is your name? ")
+    print_hello(user_name)
+
+main()
